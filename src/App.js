eba769efc6c84ead1c4d6fe144850645aa53d97f
Andrew Kerr <kerrand@protonmail.com>
2021-10-05 17:36:25 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isClicked: false};
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isClicked: !prevState.isClicked
+        }));
+    }
+
+    render() {
+        return (
+            <button className={this.state.isClicked ? 'square-active' : 'square-inactive'} onClick={this.handleClick}>
+                {this.state.isClicked ? '1' : '0'}
+            </button>
+        );
+    }
+}
+
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isToggleOn: true};
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'OFF' : 'ON'}
+            </button>
+        );
+    }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+{/*       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +64,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </div>
   );
 }
