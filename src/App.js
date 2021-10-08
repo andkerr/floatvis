5336947f7a1bedd63e0746becac6a0f12bd94805
Andrew Kerr <kerrand@protonmail.com>
2021-10-08 00:18:27 -0400

    add sign, exponent and significand bits

--- src/App.js
+++ src/App.js
@@ -11,33 +11,75 @@ class Square extends React.Component {
     }
 }
 
-class Calc extends React.Component {
+class Calculator extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            sum: 0,
-            squares: Array(this.props.length).fill(false),
+            sign_bit: 0,
+            exponent: 0,
+            significand: 0,
+            exponent_bits: Array(8).fill(false),
+            significand_bits: Array(23).fill(false)
         };
-
-        this.handleClick = this.handleClick.bind(this);
-        this.renderSquare = this.renderSquare.bind(this);
     }
 
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        squares[i] = !this.state.squares[i];
-        const sum = squares[i] ? this.state.sum + 1 : this.state.sum - 1
+    handleClickSign() {
+        const new_sign = this.state.sign_bit === 1 ? 0 : 1;
+
+        this.setState({ sign_bit: new_sign });
+    }
+
+    handleClickExponent(i) {
+        const exponents = this.state.exponent_bits.slice();
+        exponents[i] = !this.state.exponent_bits[i];
+        const len = this.props.length;
+        const exponent = exponents[i] ? this.state.exponent + 2**(len - i - 1) :
+                                      this.state.exponent - 2**(len - i - 1);
         this.setState({
-            squares: squares,
-            sum: sum
+            exponent_bits: exponents,
+            exponent: exponent
         });
     }
 
-    renderSquare(i) {
-        const isClicked = this.state.squares[i];
+    handleClickSignificand(i) {
+        const significands = this.state.significand_bits.slice();
+        significands[i] = !this.state.significand_bits[i];
+        const new_significand = significands[i] ? this.state.significand + 2**(-i - 1) :
+                                                  this.state.significand - 2**(-i - 1);
+
+        this.setState({
+            significand_bits: significands,
+            significand: new_significand
+        });
+    }
+
+    renderSign() {
+        const isClicked = this.state.sign_bit === 1;
         return (
             <Square
-                onClick={() => this.handleClick(i)}
+                onClick={() => this.handleClickSign()}
+                value={isClicked ? '1' : '0'}
+                className = {isClicked ? 'square-active' : 'square-inactive'}
+            />
+        );
+    }
+
+    renderExponent(i) {
+        const isClicked = this.state.exponent_bits[i];
+        return (
+            <Square
+                onClick={() => this.handleClickExponent(i)}
+                value={isClicked ? '1' : '0'}
+                className = {isClicked ? 'square-active' : 'square-inactive'}
+            />
+        );
+    }
+
+    renderSignificand(i) {
+        const isClicked = this.state.significand_bits[i];
+        return (
+            <Square
+                onClick={() => this.handleClickSignificand(i)}
                 value={isClicked ? '1' : '0'}
                 className = {isClicked ? 'square-active' : 'square-inactive'}
             />
@@ -45,15 +87,34 @@ class Calc extends React.Component {
     }
 
     render() {
-        const sum = this.state.sum;
+        const sign = (-1)**this.state.sign_bit;
+        const exponent = this.state.exponent;
+        const num_exponents = this.state.exponent_bits.length;
+        const bias = (2**num_exponents / 2) - 1;
+
+        var exponent_bits = [];
+        for (let i = 0; i < num_exponents; ++i) {
+            exponent_bits.push(this.renderExponent(i));
+        }
+
+        var significand_bits = [];
+        for (let i = 0; i < this.state.significand_bits.length; ++i) {
+            significand_bits.push(this.renderSignificand(i));
+        }
+
         return (
             <div>
-                <div>{sum}</div>
                 <div>
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                    {this.renderSquare(3)}
+                    {sign} x 2<sup>{exponent - bias}</sup>
+                    + {1 + this.state.significand}
+                    = {sign * 2**(exponent - bias) * (1 + this.state.significand)}
+                </div>
+                <div>
+                    {this.renderSign()}
+                    ---
+                    {exponent_bits}
+                    ---
+                    {significand_bits}
                 </div>
             </div>
         );
@@ -64,22 +125,8 @@ class Calc extends React.Component {
 function App() {
   return (
     <div className="App">
-{/*       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <div>
-          <Calc length={4} />
+          <Calculator length={8} />
       </div>
     </div>
   );
